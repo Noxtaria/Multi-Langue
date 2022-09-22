64814e61ec3f5ae6d518e3dc6bad1eba35239309
Kévin - Noxtaria <noxtariadev@gmail.com>
2022-09-22 17:16:53 +0200

    Context Provider

--- src/Components/Contenu/Contenu.js
+++ src/Components/Contenu/Contenu.js
@@ -1,12 +1,16 @@
 import React, {useContext} from 'react'
+import { Context } from '../../context/langContext'
 import './Contenu.css'
 import data from '../../assets/data.js'
 
 export default function Contenu() {
+
+    const {lang} = useContext(Context)
+
   return (
     <div className='content'>
-        <h1 className='title'>{data['FR'].title}</h1>
-        <p className='content-txt'>{data['FR'].txt}</p>
+        <h1 className='title'>{data[lang].title}</h1>
+        <p className='content-txt'>{data[lang].txt}</p>
     </div>
   )
 }
